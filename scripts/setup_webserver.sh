6138b0cfdb2e2d451a71618aaba70fff099183fb
ed-rom <devtest@ed-rom.com>
2020-04-10 10:31:07 +0100

    Update setup_webserver.sh

--- scripts/setup_webserver.sh
+++ scripts/setup_webserver.sh
@@ -358,7 +358,7 @@ EOF
    sed -i "s/;opcache.enable.*/opcache.enable = 1/" $PhpIni
    sed -i "s/;opcache.memory_consumption.*/opcache.memory_consumption = 256/" $PhpIni
    sed -i "s/;opcache.max_accelerated_files.*/opcache.max_accelerated_files = 10000/" $PhpIni
-   
+   sed -i "s/;opcache.revalidate_freq.*/opcache.revalidate_freq = 360/" $PhpIni
     
    # Remove the default site. Moodle is the only site we want
    rm -f /etc/nginx/sites-enabled/default
